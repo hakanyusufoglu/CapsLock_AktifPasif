10e6072c00cb91f54f981afb5aa72b69e4f4763e
hakanyusufoglu <hakanyusufoglu@outlook.com>
2017-06-09 19:33:29 +0300

    CapsLock Aktif Pasif Durumu

---
+++ capslockKontrolWİN/capslockKontrolWİN/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace capslockKontrolWİN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pencereyiAçToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.programıKapatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.programıKapatToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.notifyIcon2 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "CapsLock";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pencereyiAçToolStripMenuItem,
+            this.programıKapatToolStripMenuItem,
+            this.programıKapatToolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(171, 70);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // pencereyiAçToolStripMenuItem
+            // 
+            this.pencereyiAçToolStripMenuItem.Name = "pencereyiAçToolStripMenuItem";
+            this.pencereyiAçToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.pencereyiAçToolStripMenuItem.Text = "Pencereyi Aç";
+            this.pencereyiAçToolStripMenuItem.Click += new System.EventHandler(this.pencereyiAçToolStripMenuItem_Click);
+            // 
+            // programıKapatToolStripMenuItem
+            // 
+            this.programıKapatToolStripMenuItem.Name = "programıKapatToolStripMenuItem";
+            this.programıKapatToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.programıKapatToolStripMenuItem.Text = "Pencereyi Gizle";
+            this.programıKapatToolStripMenuItem.Click += new System.EventHandler(this.programıKapatToolStripMenuItem_Click);
+            // 
+            // programıKapatToolStripMenuItem1
+            // 
+            this.programıKapatToolStripMenuItem1.Name = "programıKapatToolStripMenuItem1";
+            this.programıKapatToolStripMenuItem1.Size = new System.Drawing.Size(170, 22);
+            this.programıKapatToolStripMenuItem1.Text = "Programımı Kapat";
+            this.programıKapatToolStripMenuItem1.Click += new System.EventHandler(this.programıKapatToolStripMenuItem1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(12, 28);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(138, 20);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Otomatik Açılsın";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(171, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Tamam";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(156, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // notifyIcon2
+            // 
+            this.notifyIcon2.Text = "notifyIcon2";
+            this.notifyIcon2.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = global::capslockKontrolWİN.Properties.Resources._1163135_dark_blue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(258, 120);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CapsLock Kontrol";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem pencereyiAçToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem programıKapatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem programıKapatToolStripMenuItem1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon2;
+    }
+}
+
---
+++ capslockKontrolWİN/capslockKontrolWİN/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using hakanYusufoglu;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace capslockKontrolWİN
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dinlenilecekTuslar();
+           kontrol.unhook();
+            kontrol.hook();
+            try
+            {
+                RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+                if (key.GetValue("capslockKontrolWİN").ToString() == "\"" + Application.ExecutablePath + "\"") //pc acılısında oto acılsın aktifse aktif işaretli
+                {
+                    checkBox1.Checked = true;
+                    if(checkBox1.Checked)
+                    {
+                        this.Hide();
+                    }
+                   
+                }
+                else
+                {
+                    checkBox1.Checked = false;
+                }
+            }
+            catch
+            {
+               
+            }
+
+
+
+        }
+        globalKeyboardHook kontrol = new globalKeyboardHook();
+
+        public void dinlenilecekTuslar()
+        {
+            try
+            {
+
+                kontrol.HookedKeys.Add(Keys.CapsLock);
+
+                kontrol.KeyDown += new KeyEventHandler(balon);
+                kontrol.unhook();
+
+
+
+            }
+            catch
+            {
+                MessageBox.Show("Bir Sorunla karşılaşıldı KOD=2");
+            }
+        }
+        int tester=1;
+        void balon(object sender, KeyEventArgs e)
+        {
+           
+            if (Control.IsKeyLocked(Keys.CapsLock) == true)
+
+            {
+                notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+                notifyIcon1.BalloonTipTitle = "Capslock Tuşu KAPALI";
+                notifyIcon1.BalloonTipText = "KÜÇÜK harflerle yazabilirsiniz. ";
+                notifyIcon1.ShowBalloonTip(10); //Mesajı 10sn. görüntül0
+               
+                tester = 1;
+                if (tester == 1)
+                {
+                    label1.Text = "kucuk";
+                   
+                }
+      
+
+
+
+
+                e.Handled = false;
+
+
+            }
+
+
+
+            else if (Control.IsKeyLocked(Keys.CapsLock) == false)
+            {
+
+                 notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+                 notifyIcon1.BalloonTipTitle = "Capslock Tuşu AÇIK";
+                 notifyIcon1.BalloonTipText = "BÜYÜK harflerle yazabilirsiniz";
+                 notifyIcon1.ShowBalloonTip(10); //Mesajı 10sn. görüntüler
+                
+                 e.Handled = false;
+                kontrol.hook();
+                
+                tester = 0;
+                  if (tester == 0)
+                {
+                    label1.Text = "buyk";
+                    
+                }
+                  
+                kontrol.unhook();
+            }
+          
+        }
+    
+        
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+          
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            
+                
+             
+
+            
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          if(checkBox1.Checked)
+            {
+                RegistryKey anahtar = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+                anahtar.SetValue("capslockKontrolWİN", "\"" + Application.ExecutablePath + "\"");
+                MessageBox.Show("Otomatik Açılma Aktif");
+            }
+            else
+            {
+                RegistryKey anahtar2= Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+                anahtar2.DeleteValue("capslockKontrolWİN");
+                MessageBox.Show("Otomatik Açılma Devredışı");
+            }
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            
+        }
+
+        private void pencereyiAçToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            /*this.Visible = true;*/
+            this.Show();
+        }
+
+        private void programıKapatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+          Hide();
+            
+        }
+
+        private void programıKapatToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void notifyIcon2_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
